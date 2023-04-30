84d751c5bfa38050161858012a68cdfc56f7afb8
Nazli <nazlibulbuloglu@outlook.com>
2023-04-30 22:46:55 +0300

    tamamlandi

--- src/index.js
+++ src/index.js
@@ -43,3 +43,48 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+const menuItems = document.querySelectorAll('nav a');
+menuItems.forEach((item, index)=>{
+  item.textContent= siteContent.nav[`nav-item-${index + 1}`];
+  item.classList.add("italic");
+});
+
+const mainTextContent = document.querySelectorAll(".text-content");
+ let index = 0;
+for (let i=0; i < mainTextContent.length; i++){
+  mainTextContent[i].children[0].textContent = Object.values(
+    siteContent["ana-içerik"]
+   )[index];
+
+  index +=1;
+  mainTextContent[i].children[1].textContent = Object.values(
+    siteContent["ana-içerik"]
+  )[index];
+  index += 1;
+  };
+
+  document
+  .querySelector("#logo-img")
+  .setAttribute("src", "http://localhost:9000/img/logo.png");
+  
+document.querySelector("button").textContent = "Başlayın";
+
+document.querySelector("h1").textContent = "Bu DOM Mükemmel";
+document
+  .querySelector("#cta-img")
+  .setAttribute("src", "http://localhost:9000/img/cta.png");
+
+document
+  .querySelector("#middle-img")
+  .setAttribute("src", "http://localhost:9000/img/accent.png");  
+
+const footer = document.querySelector("footer a");
+footer.textContent = siteContent.footer.copyright;
+footer.classList.add("bold");
+
+const iletisim = document.querySelectorAll("section.contact h4, section.contact p");
+
+iletisim[0].textContent = siteContent["iletisim"]["iletişim-h4"];
+iletisim[1].textContent = siteContent["iletisim"]["adres"];
+iletisim[2].textContent = siteContent["iletisim"]["telefon"];
+iletisim[3].textContent = siteContent["iletisim"]["email"];
